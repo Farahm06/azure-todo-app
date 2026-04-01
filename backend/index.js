7bd03c0e022e1eea33d6f0141bc7452cb1a79af9
Farahm06 <farahmeka16@gmail.com>
2026-04-01 23:02:48 +0200

    Change dotenv config to not override existing vars

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ override: false });
 
 const app = express();
 app.use(cors());
